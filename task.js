526d2c8916e1a3c554677d1a32774868e436b78b
Aleksandr <aleksandrnosov2008@gmail.com>
2024-01-08 20:47:43 +0300

    change task.js

--- task.js
+++ task.js
@@ -1,37 +1,35 @@
-let marks = [];
-let student1 = new Student("Вася", "мужской", 25);
 
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  return subject;
+const student5 = new Student("Anton", "male", 23);
+const student6 = new Student("Natasha", "female", 33);
+const student7 = new Student("Andrey", "male", 25);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  if(marks.length === 0 || !marks) {
-    return false;
-  } else {
-    marks.push(...arguments)
-  }
+Student.prototype.addMarks = function(...marks) {
+	if (!this.hasOwnProperty("marks")) {
+		return 0;
+	}
+	this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-    for(let i = 0; i < marks.length; i++) {
-        totalSum += marks[i];
-        if(marks.length === 0 || !marks) {
-            return 0;
-          } else {
-            let average = totalSum / marks.length;
-          }
-    }
+Student.prototype.getAverage = function() {
+	if (!this.hasOwnProperty("marks") || (this.marks.length === 0)) {
+		return 0;
+	}
+	return this.marks.reduce((acc, item) => acc += item, 0) / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  delete subject;
-  delete marks;
-  let excluded = new reason;
+Student.prototype.exclude = function(reason) {
+	this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
 }
